71cb02f9c6628e820100540f03d046f4b8c5f5f5
yuk fai chan <yukfaichan92@gmail.com>
2021-08-11 14:45:41 -0700

    leetcode problems

---
+++ javascript/Base 7.js
@@ -0,0 +1,14 @@
+// Given an integer num, return a string of its base 7 representation.
+
+var convertToBase7 = function (num) {
+    return num.toString(7)
+};
+
+//     Example 1:
+
+// Input: num = 100
+// Output: "202"
+// Example 2:
+
+// Input: num = -7
+// Output: "-10"
