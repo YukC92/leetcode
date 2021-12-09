dd17cdf6ed3636f4ec4a98502424a567f524802f
yuk fai chan <yukfaichan92@gmail.com>
2021-12-09 15:01:54 -0800

    leetcode problem

---
+++ javascript/Day of the Year.js
@@ -0,0 +1,47 @@
+// Given a string date representing a Gregorian calendar date formatted as YYYY - MM - DD, return the day number of the year.
+
+var dayOfYear = function (date) {
+    let converted = date.split("-");
+    Non_Leap_months = {
+        0: 0,
+        1: 31,
+        2: 59,
+        3: 90,
+        4: 120,
+        5: 151,
+        6: 181,
+        7: 212,
+        8: 243,
+        9: 273,
+        10: 304,
+        11: 334,
+        12: 365,
+    };
+    if (Number(converted[0]) % 4 !== 0) {
+        return Non_Leap_months[Number(converted[1]) - 1] + Number(converted[2]);
+    } else {
+        if (Number(converted[1] > 2)) {
+            return Non_Leap_months[Number(converted[1]) - 1] + Number(converted[2]) + 1;
+        } else {
+            return Non_Leap_months[Number(converted[1]) - 1] + Number(converted[2]);
+        }
+    }
+};
+
+//     Example 1:
+
+// Input: date = "2019-01-09"
+// Output: 9
+// Explanation: Given date is the 9th day of the year in 2019.
+// Example 2:
+
+// Input: date = "2019-02-10"
+// Output: 41
+// Example 3:
+
+// Input: date = "2003-03-01"
+// Output: 60
+// Example 4:
+
+// Input: date = "2004-03-01"
+// Output: 61
