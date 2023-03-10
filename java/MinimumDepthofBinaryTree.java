231b530c86ab105a19042f05b9e2ba577081812a
YukC92 <yukfaichan92@gmail.com>
2023-03-09 23:25:28 -0800

    leetcode

---
+++ java/MinimumDepthofBinaryTree.java
@@ -0,0 +1,66 @@
+// Given a binary tree, find its minimum depth.
+
+// The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+
+// Note: A leaf is a node with no children.
+
+ 
+
+// Example 1:
+
+
+// Input: root = [3,9,20,null,null,15,7]
+// Output: 2
+// Example 2:
+
+// Input: root = [2,null,3,null,4,null,5,null,6]
+// Output: 5
+ 
+
+// Constraints:
+
+// The number of nodes in the tree is in the range [0, 105].
+// -1000 <= Node.val <= 1000
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+    public int minDepth(TreeNode root) {
+        // Base case...
+        // If the subtree is empty i.e. root is NULL, return depth as 0...
+        if (root == null)
+            return 0;
+        // Initialize the depth of two subtrees...
+        int leftDepth = minDepth(root.left);
+        int rightDepth = minDepth(root.right);
+        // If the both subtrees are empty...
+        if (root.left == null && root.right == null)
+            return 1;
+        // If the left subtree is empty, return the depth of right subtree after adding
+        // 1 to it...
+        if (root.left == null)
+            return 1 + rightDepth;
+        // If the right subtree is empty, return the depth of left subtree after adding
+        // 1 to it...
+        if (root.right == null)
+            return 1 + leftDepth;
+        // When the two child function return its depth...
+        // Pick the minimum out of these two subtrees and return this value after adding
+        // 1 to it...
+        return Math.min(leftDepth, rightDepth) + 1; // Adding 1 is the current node which is the parent of the two
+                                                    // subtrees...
+    }
+}
